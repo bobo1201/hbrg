586649c2720fdf4af1a0772a112a028cd1aa250d
jeong <deas124@gmail.com>
2023-04-10 11:22:23 +0900

    Initial commit

---
+++ src/main/java/com/hbrg/HbrgApplication.java
@@ -0,0 +1,13 @@
+package com.hbrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbrg/HbrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
