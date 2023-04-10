fa0835a9d2a5d62b97e22543a98be8a7240db920
jeong <deas124@gmail.com>
2023-04-10 15:32:57 +0900

    23/04/10 15:32 pom 및 properties 수정

--- src/main/java/com/hbrg/HbrgApplication.java
+++ src/main/java/com/hbrg/HbrgApplication.java
@@ -2,7 +2,11 @@ package com.hbrg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class HbrgApplication {
 
@@ -10,4 +14,8 @@ public class HbrgApplication {
 		SpringApplication.run(HbrgApplication.class, args);
 	}
 
+	@GetMapping(value = "/")
+	public String HelloWorld(){
+		return "Hello World";
+	}
 }
