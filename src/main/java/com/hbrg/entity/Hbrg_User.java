68d93fd41e377256e9bd629863b1a92c275d8bad
Jang_John <121841741+JangJohn-Lee@users.noreply.github.com>
2023-04-11 16:32:47 +0900

    Revert "회원가입창까지 출력"
    This reverts commit 318de1fcac2bf01538b119f618569a3d03a45d18.

--- src/main/java/com/hbrg/entity/Hbrg_User.java
+++ src/main/java/com/hbrg/entity/Hbrg_User.java
@@ -1,10 +1,8 @@
 package com.hbrg.entity;
 
-import com.hbrg.dto.Hbrg_UserDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
@@ -15,43 +13,26 @@ import javax.persistence.*;
 @ToString
 public class Hbrg_User {
 
-    @Column(name="UserId")
+    @Column(name="userId")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long UserId; // 아이디 순서
+    private Long userId; // 아이디 순서
 
     @Id
-    @Column(name="Id", nullable = false)
+    @Column(name="id", nullable = false)
     private String id; // 로그인 ID
 
-    @Column(name="Pw", nullable = false)
+    @Column(name="pw", nullable = false)
     private String pw; // 로그인 PW
 
-    @Column(name="Em", nullable = false)
+    @Column(name="em", nullable = false)
     private String em; // 회원가입 이메일
 
-    @Column(name="Nic", nullable = false)
+    @Column(name="nic", nullable = false)
     private String nic; // 회원가입 닉네임
 
-    @Column(name="Ph")
+    @Column(name="ph")
     private Long ph; // 회원가입 전화번호
 
-    @Column(name="Ad")
+    @Column(name="ad")
     private String ad; // 회원가입 주소
-
-    //Hbrg_User 엔티티 생성 메소드
-    public static Hbrg_User createHbrg_User (Hbrg_UserDto hbrg_UserDto, PasswordEncoder passwordEncoder){
-        Hbrg_User hbrg_user = new Hbrg_User();
-        hbrg_UserDto.setId(hbrg_UserDto.getId());
-        hbrg_UserDto.setPw(hbrg_UserDto.getPw());
-        hbrg_UserDto.setEm(hbrg_UserDto.getEm());
-        hbrg_UserDto.setNic(hbrg_UserDto.getNic());
-        hbrg_UserDto.setPh(hbrg_UserDto.getPh());
-        hbrg_UserDto.setAd(hbrg_UserDto.getAd());
-
-        //SecurityConfig의 BCryptPasswordEncoder Bean을 파라미터로 넘겨서 비밀번호 암호화
-        String Pw = passwordEncoder.encode(hbrg_UserDto.getPw());
-        hbrg_user.setPw(Pw);
-
-        return hbrg_user;
-    }
 }
