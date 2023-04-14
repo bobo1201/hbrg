c4b81c0b3d9bb24f0d2a5dec7c0debf07921de9f
John <sangrock510@naver.com>
2023-04-14 11:06:56 +0900

    0414/11:06
    회원가입시 로그인화면으로 리다이렉트

--- src/main/java/com/hbrg/controller/UserController.java
+++ src/main/java/com/hbrg/controller/UserController.java
@@ -48,8 +48,8 @@ public class UserController {
         }catch(IllegalStateException e){
             model.addAttribute("errorMessage", e.getMessage());
         }
-
-        return "redirect:/";
+        //회원가입 성공시 로그인 페이지로 이동
+        return "redirect:/user/login";
     }
 
     @GetMapping(value="/login")
