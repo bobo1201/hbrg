c3cf4c2818f20ca4e1498e01354669d23be32d4b
jeong <wjdqhtjd12@naver.com>
2023-04-14 18:21:51 +0900

    config 추가

--- src/main/java/com/hbrg/controller/BoardController.java
+++ src/main/java/com/hbrg/controller/BoardController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="/Hbrg")
+@RequestMapping(value="/hbrg")
 public class BoardController {
 
 
-    @GetMapping(value = "/Board")
+    @GetMapping(value = "/board")
     public String Board() {
         return "Content";
     }
